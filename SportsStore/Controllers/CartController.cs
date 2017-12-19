a128bd9fb13cf32ca642c251d73c5ac87526b91b
kephamose <kepham@gmail.com>
2017-12-19 17:54:57 -0600

    Chapter 10

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -12,17 +12,19 @@ namespace SportsStore.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
+        private Cart cart;
 
-        public CartController(IProductRepository repo)
+        public CartController(IProductRepository repo, Cart cartService)
         {
             repository = repo;
+            cart = cartService;
         }
 
         public ViewResult Index(string returnUrl)
         {
             return View(new CartIndexViewModel
             {
-                Cart = GetCart(),
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
         }
@@ -34,37 +36,24 @@ namespace SportsStore.Controllers
 
             if (product != null)
             {
-                Cart cart = GetCart();
                 cart.AddItem(product, 1);
-                SaveCart(cart);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
 
-        //public RedirectToActionResult RemoveFromCart(int productId,
-        //    string returnUrl)
-        //{
-        //    Product product = repository.Products
-        //        .FirstOrDefault(p => p.ProductID == productId);
-
-        //    if (product != null)
-        //    {
-        //        Cart cart = GetCart();
-        //        cart.RemoveLine(product);
-        //        SaveCart(cart);
-        //    }
-        //    return RedirectToAction("Index", new { returnUrl });
-        //}
-
-        private Cart GetCart()
+        public RedirectToActionResult RemoveFromCart(int productId,
+            string returnUrl)
         {
-            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-            return cart;
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                cart.RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
         }
 
-        private void SaveCart(Cart cart)
-        {
-            HttpContext.Session.SetJson("Cart", cart);
-        }
+        
     }
 }
