f7be86d3183993c4f65e5bf53c777d17e9f22acc
kephamose <kepham@gmail.com>
2017-12-19 21:53:14 -0600

    Chapter 11

--- SportsStore/Controllers/OrderController.cs
+++ SportsStore/Controllers/OrderController.cs
@@ -18,6 +18,21 @@ namespace SportsStore.Controllers
             cart = cartService;
         }
 
+        public ViewResult List() =>
+            View(repository.Orders.Where(o => !o.Shipped));
+        [HttpPost]
+        public IActionResult MarkShipped(int orderID)
+        {
+            Order order = repository.Orders
+                .FirstOrDefault(o => o.OrderID == orderID);
+            if (order != null)
+            {
+                order.Shipped = true;
+                repository.SaveOrder(order);
+            }
+            return RedirectToAction(nameof(List));
+        }
+
         public ViewResult Checkout() => View(new Order());
 
         [HttpPost]
@@ -38,6 +53,7 @@ namespace SportsStore.Controllers
                 return View(order);
             }
         }
+
         public ViewResult Completed()
         {
             cart.Clear();
